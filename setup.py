db461262888dd2fc0f40a123e2e143ed7ccaa249
tripleee <tripleee@users.noreply.github.com>
2019-07-19 10:33:07 +0300

    Release 0.7.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup (
     name = "BotpySE",
     packages = ["BotpySE"],
-    version = "0.7.7",
+    version = "0.7.8",
     description = "A python framework to create chatbots on the StackExchange network.",
     author = "Ashish Ahuja",
     author_email = "ashish.ahuja@sobotics.org",
